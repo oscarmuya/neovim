57e6286bd724e3cfd2af1f6839c21d87cf34494b
Oscar Muya <oscarmuyalx@gmail.com>
2026-02-20 18:03:13 +0300

    feat: add Go language support

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.go" },
 
     -- import/override with your plugins
     { import = "plugins" },
