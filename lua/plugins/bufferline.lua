a68011fda8f54c4bad05e488dde0d371ab256713
Oscar Muya <oscarmuyalx@gmail.com>
2026-02-20 18:03:20 +0300

    refactor: simplify bufferline configuration

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,20 +1,3 @@
--- return {
---   {
---     "akinsho/bufferline.nvim",
---     opts = {
---       options = {
---         mode = "tabs",
---         show_buffer_close_icons = false,
---         show_close_icon = false,
---       },
---     },
---     keys = {
---       { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
---       { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Previous tab" },
---     },
---   },
--- }
-
 return {
   {
     "akinsho/bufferline.nvim",
@@ -23,18 +6,6 @@ return {
         mode = "tabs",
         show_buffer_close_icons = false,
         show_close_icon = false,
-        indicator = { style = "none" },
-        -- separator_style = "slant",
-      },
-      highlights = {
-        fill = { bg = "#1e1e2e" },
-        background = { bg = "#1e1e2e", fg = "#585b70" },
-        tab = { bg = "#1e1e2e", fg = "#585b70" },
-        tab_selected = { bg = "#313244", fg = "#cdd6f4", bold = true },
-        tab_separator = { bg = "#1e1e2e", fg = "#1e1e2e" },
-        tab_separator_selected = { bg = "#313244", fg = "#1e1e2e" },
-        buffer_selected = { bg = "#313244", fg = "#cdd6f4", bold = true },
-        indicator_selected = { bg = "#313244", fg = "#89b4fa" },
       },
     },
     keys = {
@@ -43,3 +14,32 @@ return {
     },
   },
 }
+
+-- return {
+--   {
+--     "akinsho/bufferline.nvim",
+--     opts = {
+--       options = {
+--         mode = "tabs",
+--         show_buffer_close_icons = false,
+--         show_close_icon = false,
+--         indicator = { style = "none" },
+--         -- separator_style = "slant",
+--       },
+--       highlights = {
+--         fill = { bg = "#1e1e2e" },
+--         background = { bg = "#1e1e2e", fg = "#585b70" },
+--         tab = { bg = "#1e1e2e", fg = "#585b70" },
+--         tab_selected = { bg = "#313244", fg = "#cdd6f4", bold = true },
+--         tab_separator = { bg = "#1e1e2e", fg = "#1e1e2e" },
+--         tab_separator_selected = { bg = "#313244", fg = "#1e1e2e" },
+--         buffer_selected = { bg = "#313244", fg = "#cdd6f4", bold = true },
+--         indicator_selected = { bg = "#313244", fg = "#89b4fa" },
+--       },
+--     },
+--     keys = {
+--       { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+--       { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Previous tab" },
+--     },
+--   },
+-- }
