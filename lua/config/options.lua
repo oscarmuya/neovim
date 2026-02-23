f6c6cede06d80aed2997a5e0dc04729be93d05ad
Oscar Muya <oscarmuyalx@gmail.com>
2026-02-24 01:53:57 +0300

    style(options): set editor-wide 2-space indentation settings

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,3 +8,9 @@ vim.opt.fillchars:append({ eob = "~" })
 
 --- disable netrw banner
 vim.g.netrw_banner = 0
+
+---- spaces for tabs
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
