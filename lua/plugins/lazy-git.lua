29b8bef8a2bafe071b69db0b51c358f3cd72da0f
Oscar Muya <oscarmuyalx@gmail.com>
2026-02-25 20:21:27 +0300

    feat: Disable neotree and lazy git.

---
+++ lua/plugins/lazy-git.lua
@@ -0,0 +1,17 @@
+return {
+  { "kdheepak/lazygit.nvim", enabled = false },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      lazygit = { enabled = false },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      defaults = {
+        keymaps = false,
+      },
+    },
+  },
+}
